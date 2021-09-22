f429aec8f00e7770867a313212113f548167d524
Paulina Acosta <paulinaacostac@hotmail.com>
2021-09-22 17:44:45 -0400

    Until Episode 3

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -5,17 +5,22 @@ import com.codepath.apps.restclienttemplate.TimeFormatter;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.time.temporal.Temporal;
 import java.util.ArrayList;
 import java.util.List;
 
 
+@Parcel
 public class Tweet {
     public String body;
     public String createdAt;
     public User user;
     public long id;
+
+    //empty constructor needed by Parceler (for intent passing complex data)
+    public Tweet(){}
 
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException
     {
