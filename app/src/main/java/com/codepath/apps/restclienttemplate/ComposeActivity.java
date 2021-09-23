bf9b09440b37ae182bc261c0e901057a01f92904
Paulina Acosta <paulinaacostac@hotmail.com>
2021-09-23 18:16:02 -0400

    Twitter Unit 4 Readme

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -2,12 +2,17 @@ package com.codepath.apps.restclienttemplate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
@@ -20,11 +25,13 @@ import okhttp3.Headers;
 
 public class ComposeActivity extends AppCompatActivity {
     public static final String TAG = "ComposeActivity";
-    public static final int MAX_TWEET_LENGTH = 140;
+    public static final int MAX_TWEET_LENGTH = 10;
     EditText etCompose;
     Button btnTweet;
-
+    TextView tvLetterCount;
     TwitterClient client;
+    Drawable bkrColor = btnTweet.getBackground();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,9 +41,12 @@ public class ComposeActivity extends AppCompatActivity {
 
         etCompose = findViewById(R.id.etCompose);
         btnTweet = findViewById(R.id.btnTweet);
+        tvLetterCount = findViewById(R.id.tvLetterCount);
+        String concat = "0  / "+MAX_TWEET_LENGTH;
+        tvLetterCount.setText(concat);
 
 
-        //Set click listener on button
+        //Set click listener on button for submitting a new tweet
         btnTweet.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -82,6 +92,50 @@ public class ComposeActivity extends AppCompatActivity {
                 });
             }
         });
+
+        //Add text listener to the text box for the word count
+        etCompose.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                // Fires right as the text is being changed (even supplies the range of text)
+            }
+
+
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count,
+                                          int after) {
+                // Fires right before text is changing
+            }
+
+            @SuppressLint("ResourceAsColor")
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Fires right after the text has changed
+                String lengthCharsTextBox=String.valueOf(s.toString().length());
+
+
+                if(Integer.parseInt(lengthCharsTextBox) >= MAX_TWEET_LENGTH)
+                {
+                    btnTweet.setBackgroundColor(android.R.color.darker_gray);
+
+                    //Block the button too
+                    btnTweet.setEnabled(false);
+                }
+                else
+                {
+                    btnTweet.setBackground(bkrColor);
+
+                    //Block the button too
+                    btnTweet.setEnabled(true);
+                }
+                String concat = lengthCharsTextBox + " / "+MAX_TWEET_LENGTH;
+                tvLetterCount.setText(concat);
+
+            }
+
+        });
+
 
     }
 }
